083ea064316323483c07079825db92ae15e78457
Taro Morita <morita0079@gmail.com>
2020-08-11 11:22:07 +0900

    fix

--- tempsubmit.py
+++ tempsubmit.py
@@ -3,22 +3,51 @@ import time
 import os
 import csv
 import requests
-import datatime
+import datetime
 
 CLK = 24
 CS = 4
 DO1 = 25
-
+url = 'https://recievetempflaskapi.herokuapp.com/post'
 unit = 'c'
 
 thermocouple = MAX6675.MAX6675(CLK, CS,DO1, unit)
 
+# csv file confirmation
+
+
 time.sleep(1)
 running = True
-
+starttime = time.time() # get start time
 while(running):
     try:
-        temp = thermocouple.get_temp()
+        nowtimeforfile = datetime.datetime.now()
+        fileName = str(nowtimeforfile.year) + '-' + str(nowtimeforfile.month) + '-' + str(nowtimeforfile.day) + '-data.csv'
+        nowtime = time.time() # get now time
+        elapsedtime = round(nowtime - starttime) # elapsed time 
+        divtime = 0.5 # minuites
+
+        if elapsedtime % (divtime * 60) == 0:
+            response = requests.post(url, data= {'test':'test'})
+            print(response.status_code)
+            print(response.text)
+            dtNow = datetime.datetime.now()
+            temp = thermocouple.get_temp()
+            data = [dtNow, temp]
+
+            # csv file make
+            if os.path.exists(fileName) == False:
+                with open(fileName, 'w') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(['date', 'temperature'])
+            else:
+                with open(fileName, 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(data)
+
+
+
+
 
         time.sleep(1)
     except KeyboardInterrupt:
